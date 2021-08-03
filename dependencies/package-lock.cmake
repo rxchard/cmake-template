b0b3300d9260f7e66e226386c1fb569b6e1da951
rxchard <40161763+rxchard@users.noreply.github.com>
2021-08-03 20:33:33 +0200

    template commit

---
+++ dependencies/package-lock.cmake
@@ -0,0 +1,8 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+CPMDeclarePackage(
+  fmt
+  GIT_TAG 07211701f475b1148a7634afa8d6dacb1feff4ee
+  GITHUB_REPOSITORY fmtlib/fmt
+)
